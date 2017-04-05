8ea29dd5cd1ff703f7b621c23cc853ad813e9680
JasonDeveloper <jasondreamxu@163.com>
2017-04-05 12:44:52 +0800

    添加通知、全局常量配置文件

---
+++ NeiHanJoke/Config/NHNotificationContants.h
@@ -0,0 +1,15 @@
+//
+//  NHNotificationContants.h
+//  NeiHanJoke
+//
+//  Created by Jason_Xu on 2017/4/5.
+//  Copyright © 2017年 Jason_Xu. All rights reserved.
+//
+
+#ifndef NHNotificationContants_h
+#define NHNotificationContants_h
+/*  */
+static NSString *const notification = @"";
+
+
+#endif /* NHNotificationContants_h */
